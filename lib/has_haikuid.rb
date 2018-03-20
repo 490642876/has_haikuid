4daa4c9b9a2d957543ce042f1380a57d22a327bc
lizl <490642876@qq.com>
2018-03-20 17:35:50 +0800

    增加重复判断

--- lib/has_haikuid.rb
+++ lib/has_haikuid.rb
@@ -7,13 +7,24 @@ module ActiveRecord
 
     module ClassMethods
 
-      def has_haikuid(attribute = :id)
+      def has_haikuid(attribute = :id, token_range: 9999, delimiter: "-")
         require 'haikunator'
-        before_create { self.send("#{attribute}=", self.class.generate_haikuid) unless self.send("#{attribute}?")}
+        before_create do
+          unless self.send("#{attribute}?")
+            begin
+              retries ||= 0
+              haikuid = self.class.generate_haikuid(token_range, delimiter)
+              raise "repeat" if self.class.exists?(attribute.to_sym => haikuid)
+              self.send("#{attribute}=", haikuid)
+            rescue
+              retry if (retries += 1) < 3
+            end
+          end
+        end
       end
 
-      def generate_haikuid
-        Haikunator.haikunate
+      def generate_haikuid(token_range = 9999, delimiter = '-')
+        Haikunator.haikunate(token_range, delimiter)
       end
     end
   end
--- test/models/user.rb
+++ test/models/user.rb
@@ -1,4 +1,4 @@
 class User < ActiveRecord::Base
   has_haikuid
-  has_haikuid :oid
+  has_haikuid :oid, token_range: 999999
 end
